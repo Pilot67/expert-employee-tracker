4cd4b7551daa437e130439783692c254c65b3092
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-09 10:45:42 +1100

    added file structure

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ("Accounting"),
+("Maintenance"),
+("Corporate"),
+("IT"),
+("Retail");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Fitter", 47000, 1),
+("Senior Accountant", 130000, 0),
+("Store Manager", 83000, 4),
+("Assistant", 43000, 4),
+("Electrician", 55000, 1),
+("Web Developer", 99000, 3),
+("Front End Developer", 111000, 3)
+("HR Manager", 127000, 2);
+
