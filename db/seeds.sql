6c86456dadde7de2507816c883e6aec231344378
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-09 13:39:33 +1100

    Added query.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,13 +5,25 @@ VALUES ("Accounting"),
 ("IT"),
 ("Retail");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Fitter", 47000, 1),
-("Senior Accountant", 130000, 0),
-("Store Manager", 83000, 4),
-("Assistant", 43000, 4),
-("Electrician", 55000, 1),
-("Web Developer", 99000, 3),
-("Front End Developer", 111000, 3)
-("HR Manager", 127000, 2);
+INSERT INTO role 
+    (title, salary, department_id)
+VALUES
+    ("Fitter", 47000, 2),
+    ("Senior Accountant", 130000, 1),
+    ("Store Manager", 83000, 5),
+    ("Assistant", 43000, 5),
+    ("Electrician", 55000, 2),
+    ("Web Developer", 99000, 4),
+    ("Front End Developer", 111000, 4),
+    ("HR Manager", 127000, 3),
+    ("CEO",200000,3);
 
+INSERT INTO employee 
+    (first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Wayne", "Evans", 9, NULL),
+    ("Stuart", "Simmons", 5, 1),
+    ("John", "Smith", 6, NULL),
+    ("Patty", "Wagstaff", 7, 3),
+    ("Jeff", "Johnson", 1, 2),
+    ("Benn", "Benn", 8, NULL);
