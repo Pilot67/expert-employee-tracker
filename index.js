c631d8e02dba6d076f1b516b505f024498c14966
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-10 13:54:36 +1100

    Add department added

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ db.connect(err => {
 
 function mainMenu() {
     //console.clear();
-    inquirer.prompt(
+    inquirer.prompt([
         {
             name: "menuSelect",
             type: "list",
@@ -53,7 +53,7 @@ function mainMenu() {
                 menuPrompt.exit
                 ]
         }
-    )
+    ])
     .then(({menuSelect})=> {
         console.log(menuSelect);
         switch (menuSelect) {
@@ -62,6 +62,15 @@ function mainMenu() {
                 break;
             case menuPrompt.allRoles:
                 renderAllRoles();
+                break;
+            case menuPrompt.allEmployees:
+                renderAllEmployees();
+                break;
+            case menuPrompt.adddDept:
+                addDept();
+                break;
+            case menuPrompt.addRole:
+                addRole();
                 break;
             case menuPrompt.exit:
                 db.end()
@@ -72,12 +81,10 @@ function mainMenu() {
     })
 };
 
-async function renderAllDepts() {
+function renderAllDepts() {
     console.clear()
     console.log("View all Departments\n")
-    const sql = `SELECT department.id as ID, department.name as Department
-    FROM department
-    ORDER BY id;`
+    const sql = `SELECT department.id as ID, department.name as Department FROM department ORDER BY id;`
     return db.promise().query(sql)
     .then(([rows,fields]) => {
         console.table(rows)
@@ -86,13 +93,29 @@ async function renderAllDepts() {
     .catch((error) => console.log(error))
     .then(() => mainMenu())
 };
-async function renderAllRoles() {
+function renderAllRoles() {
     console.clear()
     console.log("View all Roles\n")
-    const sql = `SELECT role.id as ID, role.title as Title, role.salary as Salary, department.name as Department
+    const sql = `SELECT role.title as Title, role.id as ID, department.name as Department, role.salary as Salary
     FROM role
     JOIN department on role.department_id = department.id
-    ORDER BY id;`
+    ORDER BY Title;`
+    return db.promise().query(sql)
+    .then(([rows,fields]) => {
+        console.table(rows)
+    })
+    .catch((error) => console.log(error))
+    .then(() => mainMenu())
+};
+function renderAllEmployees() {
+    console.clear()
+    console.log("View all Roles\n")
+    const sql = `SELECT employee.id as ID, employee.first_name as 'First Name', employee.last_name as 'Last Name', role.title as Title, department.name as Department, role.salary as Salary, concat(manager.first_name, ' ' ,  manager.last_name) as Manager
+    FROM employee
+    JOIN role ON employee.role_id = role.id
+    JOIN department on role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id
+    ORDER BY ID;`
     return db.promise().query(sql)
     .then(([rows,fields]) => {
         console.table(rows)
@@ -101,4 +124,68 @@ async function renderAllRoles() {
     .then(() => mainMenu())
 };
 
+function addDept() {
+    console.clear();
+    console.log('Adding a new Department');
+    inquirer.prompt([
+        {
+            name: "deptName",
+            type: "input",
+            message:"What is the department Name"
+        }
+    ])
+    .then(({deptName}) => {
+        console.log(`Adding ${deptName} to Departments`)
+        const sql = `INSERT INTO department (name) VALUES (?)`
+        return db.promise().query(sql,deptName)
+        .then((res, err) => {
+            console.log('New department Added')
+        })
+        .catch((err) => console.status('500',err));
+    })
+    .then(() => {
+        mainMenu();
+    });
+};
 
+function addRole() {
+    console.clear();
+    console.log('Adding a new Role');
+    const sql = `SELECT department.name as Department, department.id as ID FROM department ORDER BY department.id;`
+    return db.promise().query(sql)
+    .then(([rows,fields]) => {
+        const deptList = rows.map((item) => item.Department)
+        inquirer.prompt([
+            {
+                name: "newRole",
+                type: "input",
+                message: "Input the new role"
+            },
+            {
+                name: "newSalary",
+                type: "input",
+                message: "Input the salary of the new role",
+            },
+            {
+                name: "department",
+                type: "list",
+                message:"Choose from the list the new role's department",
+                choices: deptList
+            },
+        ])
+        .then((answers) => {
+            const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`
+            const params = [answers.newRole.trim(),answers.newSalary,deptList.indexOf(answers.department)+1]
+            return db.promise().query(sql,params)
+            .then((res, err) => {
+                console.log('New Role Added \n')
+            })
+            .catch((err) => console.log('500',err))
+            .then(() => {
+                mainMenu()
+            });
+            
+        })
+    })
+    .catch((err) => console.log('500',err))
+}
