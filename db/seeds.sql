cef3f72930a92d051af8c8981b528d36b24f3bc7
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-12 14:00:33 +1100

    Reseeded the db and tidied up js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,34 @@
 INSERT INTO department (name)
-VALUES ("Accounting"),
+VALUES ("Corporate"),
+("Accounting"),
+("Human Resources"),
 ("Maintenance"),
-("Corporate"),
 ("IT"),
-("Retail");
+("Retail"),
+("Warehouse");
 
 INSERT INTO role 
     (title, salary, department_id)
 VALUES
-    ("Fitter", 47000, 2),
-    ("Senior Accountant", 130000, 1),
-    ("Store Manager", 83000, 5),
-    ("Assistant", 43000, 5),
-    ("Electrician", 55000, 2),
-    ("Web Developer", 99000, 4),
-    ("Front End Developer", 111000, 4),
-    ("HR Manager", 127000, 3),
-    ("CEO",200000,3);
+    ("CEO",200000,1),
+    ("Fitter", 47000, 4),
+    ("Electrician", 55000, 4),
+    ("Mechanic",62000,4),
+    ("Senior Accountant", 130000, 2),
+    ("Accountant", 85000,2),
+    ("Store Manager", 83000, 6),
+    ("Assistant Sales", 43000, 6),
+    ("Web Developer", 99000, 5),
+    ("Front End Developer", 111000, 5),
+    ("HR Manager", 127000, 3);
 
 INSERT INTO employee 
     (first_name, last_name, role_id, manager_id)
 VALUES 
-    ("Wayne", "Evans", 9, NULL),
-    ("Stuart", "Simmons", 5, 1),
-    ("John", "Smith", 6, NULL),
-    ("Patty", "Wagstaff", 7, 3),
-    ("Jeff", "Johnson", 1, 2),
-    ("Benn", "Benn", 8, NULL);
+    ("Stuart", "Simmons", 1, NULL),
+    ("Wayne", "Evans", 9, 1),
+    ("Patty", "Wagstaff", 5, 1),
+    ("John", "Smith", 6, 3),
+    ("Jeff", "Johnson", 11, 2),
+    ("Monty", "Burns",8, 4),
+    ("Benn", "Benn", 7, 6);
